10c49fa8d84f08b06d440717241c3ffd2a8176d4
kboets <k.boets@gmail.com>
2026-05-10 07:11:26 +0200

    update testcontainers configuration (exclude mongo db container) + added angular build steps

--- api/src/test/java/boets/be/nbts/TestcontainersConfiguration.java
+++ api/src/test/java/boets/be/nbts/TestcontainersConfiguration.java
@@ -10,11 +10,11 @@ import org.testcontainers.utility.DockerImageName;
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
 
-	@Bean
-	@ServiceConnection
-	MongoDBContainer mongoDbContainer() {
-		return new MongoDBContainer(DockerImageName.parse("mongo:latest"));
-	}
+//	@Bean
+//	@ServiceConnection
+//	MongoDBContainer mongoDbContainer() {
+//		return new MongoDBContainer(DockerImageName.parse("mongo:latest"));
+//	}
 
 	@Bean
 	@ServiceConnection
