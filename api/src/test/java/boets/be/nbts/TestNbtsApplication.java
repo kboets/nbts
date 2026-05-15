f0e2d44ec2169cdd055c4ada5566c3cd3c2dbf1e
kboets <k.boets@gmail.com>
2026-05-15 07:36:32 +0200

    [Leagues] - Create the league schema: update url + docker compose config

--- api/src/test/java/boets/be/nbts/TestNbtsApplication.java
+++ api/src/test/java/boets/be/nbts/TestNbtsApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 
 public class TestNbtsApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.from(NbtsApplication::main).with(TestcontainersConfiguration.class).run(args);
 	}
 
