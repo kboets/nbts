3ffe480bbc07f20287e76b6b253638fcdb0d480d
kboets <k.boets@gmail.com>
2026-06-24 07:14:57 +0200

    [Leagues] - create the league page: retrieve leagues for country

--- api/src/main/java/boets/be/nbts/leagues/web/LeagueClientService.java
+++ api/src/main/java/boets/be/nbts/leagues/web/LeagueClientService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
@@ -16,6 +17,23 @@ public class LeagueClientService extends RapidApiClient {
         super(restClientBuilder, apiKey);
     }
 
+    public List<League> getLeaguesByCountry(String countryCode) {
+        LeagueApiResponse apiResponse = get("/v3/leagues", LeagueApiResponse.class, Map.of(
+                "code", countryCode
+        ));
+        return apiResponse.response()
+                .stream()
+                .filter(item -> "League".equalsIgnoreCase(item.league().type()))
+                // only first 3 leagues should be given
+                .limit(3)
+                .flatMap(item -> item.seasons()
+                        .stream()
+                        .filter(LeagueApiResponse.LeagueApiItem.LeagueApiLeague.LeagueApiCountry.LeagueApiSeason::current)
+                        // only league of type League allowed
+                        .filter(apiSeason -> apiSeason.end().isAfter(LocalDate.now()))
+                        .map(apiSeason -> mapToLeague(item, apiSeason)))
+                .toList();
+    }
     public List<League> getLeaguesByCountryAndSeason(String countryCode, int season) {
         LeagueApiResponse apiResponse = get("/v3/leagues", LeagueApiResponse.class, Map.of(
                 "code", countryCode,
@@ -24,19 +42,26 @@ public class LeagueClientService extends RapidApiClient {
 
         return apiResponse.response()
                 .stream()
+                .filter(item -> "League".equalsIgnoreCase(item.league().type()))
+                .limit(3)
                 .flatMap(item -> item.seasons()
                         .stream()
-                        .map(apiSeason -> new League(
-                                item.league().id(),
-                                item.league().name(),
-                                item.league().logo(),
-                                item.country().code(),
-                                apiSeason.year(),
-                                apiSeason.start(),
-                                apiSeason.end(),
-                                apiSeason.current()
-                        )))
+                        .map(apiSeason -> mapToLeague(item, apiSeason)))
                 .toList();
+    }
+
+    private League mapToLeague(LeagueApiResponse.LeagueApiItem item,
+                               LeagueApiResponse.LeagueApiItem.LeagueApiLeague.LeagueApiCountry.LeagueApiSeason apiSeason) {
+        return new League(
+                item.league().id(),
+                item.league().name(),
+                item.league().logo(),
+                item.country().code(),
+                apiSeason.year(),
+                apiSeason.start(),
+                apiSeason.end(),
+                apiSeason.current()
+        );
     }
 
 }
